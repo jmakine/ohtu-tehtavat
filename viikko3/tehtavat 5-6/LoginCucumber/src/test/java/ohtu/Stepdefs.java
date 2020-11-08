c20d8d206178cb830183d7fdfc1ece8c957097b7
Jenni Mäkinen <jenni.makinen70@gmail.com>
2020-11-08 20:31:55 +0200

    lisätty stepit new-userille

--- viikko3/tehtavat 5-6/LoginCucumber/src/test/java/ohtu/Stepdefs.java
+++ viikko3/tehtavat 5-6/LoginCucumber/src/test/java/ohtu/Stepdefs.java
@@ -17,13 +17,42 @@ public class Stepdefs {
     UserDao userDao;
     AuthenticationService auth;
     List<String> inputLines;
-    
+        
     @Before
     public void setup(){
         userDao = new InMemoryUserDao();
         auth = new AuthenticationService(userDao);
-        inputLines = new ArrayList<>();      
+        inputLines = new ArrayList<>();   
     }
+    
+    @Given("^command new is selected$")
+    public void commandNewSelected() throws Throwable {
+        inputLines.add("new");
+    }
+    
+    @When("user {string} with password {string} is created")
+    public void usernameAndPasswordAreCreated(String username, String password) {
+       if(auth.createUser(username, password)){
+       inputLines.add(username);
+       inputLines.add(password);
+       
+       io = new StubIO(inputLines); 
+       app = new App(io, auth);
+       auth.createUser(username, password);
+       app.run();
+       } 
+    }
+    
+    /*@Given("user {string} with password {string} is created")
+    public void usernameAndPasswordCreated(String username, String password) throws Throwable {
+       inputLines.add("new");
+       inputLines.add(username);
+       inputLines.add(password);
+       
+       io = new StubIO(inputLines); 
+       app = new App(io, auth);
+       auth.createUser(username, password);       
+    }*/
     
     @Given("^command login is selected$")
     public void commandLoginSelected() throws Throwable {
@@ -38,11 +67,17 @@ public class Stepdefs {
        io = new StubIO(inputLines); 
        app = new App(io, auth);
        app.run();
-    }    
+    }           
     
     @Then("system will respond with {string}")
     public void systemWillRespondWith(String expectedOutput) {
         assertTrue(io.getPrints().contains(expectedOutput));
     }    
-
+    
+    /*Scenario: can login with successfully generated account
+        Given user "eero" with password "salainen1" is created
+        And   command login is selected
+        When  username "eero" and password "salainen1" is entered
+        Then  system will respond with "logged in"*/
+    
 }

2e774397ecdfeab1072f7ae83eb7f848df283ce7
Jenni Mäkinen <jenni.makinen70@gmail.com>
2020-11-10 07:07:12 +0200

    otettu HtmlUnitDriver käyttöön ja lisätty välitulostuksia

--- viikko3/tehtavat 7-11/WebLogin/src/main/java/ohtu/Tester.java
+++ viikko3/tehtavat 7-11/WebLogin/src/main/java/ohtu/Tester.java
@@ -4,19 +4,28 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class Tester {
-
+   
     public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
-
+        WebDriver driver = new HtmlUnitDriver();
+        //WebDriver driver = new ChromeDriver();
         driver.get("http://localhost:4567");
+        
+        // tulostetaan sivu konsoliin
+        System.out.println("Tulostetaan aloitussivu");
+        System.out.println(driver.getPageSource());
         
         sleep(2);
         
         WebElement element = driver.findElement(By.linkText("login"));
         element.click();
 
+        // tulostetaan sivu konsoliin
+        System.out.println("Tulostetaan 'login' sivu");
+        System.out.println(driver.getPageSource());
+        
         sleep(2);
 
         element = driver.findElement(By.name("username"));
@@ -28,6 +37,10 @@ public class Tester {
         sleep(2);
         element.submit();
 
+        // tulostetaan sivu konsoliin
+        System.out.println("Tulostetaan sivu, kun ollaan kirjoitettu username ja password ja painettu 'login'");        
+        System.out.println(driver.getPageSource());
+        
         sleep(3);
         
         driver.quit();
